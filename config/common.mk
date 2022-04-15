10e992d34578c1af571204c9ebe4caee8f953c16
johnmart19 <johnivan19999@gmail.com>
2022-04-15 10:38:35 +0300

    Revert "vendor: Build ThemeIcons for Pixel Launcher"
    This reverts commit 89066a1317222eeee0cc742b7e9119a62454b4c2.
    
    - We have better implementation in rro_overlays/CustomPixelLauncherOverlay , so drop unneeded duplicate

--- config/common.mk
+++ config/common.mk
@@ -252,7 +252,4 @@ $(call inherit-product, vendor/aosp/config/ota.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
-# Themed icons
-$(call inherit-product, packages/overlays/ThemeIcons/config.mk)
-
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
