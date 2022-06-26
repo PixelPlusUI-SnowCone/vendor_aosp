88a723099644ecd4746cbd775182086f506a201e
Master <mhmdeveloperofficial@gmail.com>
2022-06-26 23:07:37 +0530

    config: Add flag to Enable blur in UI

--- config/common.mk
+++ config/common.mk
@@ -239,10 +239,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GameSpace \
 
-# Blurs
+# UI Blur Support
+ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+endif
 
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
